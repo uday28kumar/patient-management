aa11e282340b518ad6be29eee167d9c09a1b0eaf
Uday Kumar <uday28kumar@gmail.com>
2025-12-25 17:23:13 +0530

    added docker config

--- patient-service/src/main/java/com/neev/patient_service/PatientServiceApp.java
+++ patient-service/src/main/java/com/neev/patient_service/PatientServiceApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PatientServiceApplication {
+public class PatientServiceApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PatientServiceApplication.class, args);
+		SpringApplication.run(PatientServiceApp.class, args);
 	}
 
 }
