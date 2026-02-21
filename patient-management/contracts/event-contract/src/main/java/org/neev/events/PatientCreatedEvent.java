9b52e074dbdf052ff6e6e0019af051940fbf0699
Uday Kumar <uday28kumar@gmail.com>
2026-02-21 16:53:14 +0530

    enhance documentation for BillingService, BillingRequest, BillingResponse, PatientCreatedEvent, and SerializationUtil classes

--- patient-management/contracts/event-contract/src/main/java/org/neev/events/PatientCreatedEvent.java
+++ patient-management/contracts/event-contract/src/main/java/org/neev/events/PatientCreatedEvent.java
@@ -2,6 +2,11 @@ package org.neev.events;
 
 import java.io.Serializable;
 
+/**
+ * Event representing the creation of a new patient. This event is published to Kafka when a new patient is created
+ * in the Patient Service, allowing other services (like Analytics Service) to react to this event and update their
+ * data accordingly.
+ */
 public record PatientCreatedEvent(
         String patientId,
         String name
--- patient-management/contracts/util-contract/src/main/java/org/neev/utils/SerializationUtil.java
+++ patient-management/contracts/util-contract/src/main/java/org/neev/utils/SerializationUtil.java
@@ -2,8 +2,23 @@ package org.neev.utils;
 
 import java.io.*;
 
+/**
+ * Utility class for serializing and deserializing objects to and from byte arrays. This is useful for sending
+ * complex objects through Kafka or storing them in a binary format.
+ */
 public class SerializationUtil {
 
+    private SerializationUtil() {
+        // Private constructor to prevent instantiation
+    }
+
+    /**
+     * Serializes an object to a byte array.
+     *
+     * @param obj the object to serialize
+     * @return a byte array representing the serialized object
+     * @throws IOException if an I/O error occurs during serialization
+     */
     public static byte[] serialize(Object obj) throws IOException {
 
         ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
@@ -15,6 +30,14 @@ public class SerializationUtil {
         return byteStream.toByteArray();
     }
 
+    /**
+     * Deserializes an object from a byte array.
+     *
+     * @param data the byte array containing the serialized object
+     * @return the deserialized object
+     * @throws IOException            if an I/O error occurs during deserialization
+     * @throws ClassNotFoundException if the class of the serialized object cannot be found
+     */
     public static Object deserialize(byte[] data) throws IOException, ClassNotFoundException {
 
         ByteArrayInputStream byteStream = new ByteArrayInputStream(data);
