5a5e4dc3458b4ebdb147d73f2ff4b1598f38a69a
Uday Kumar <uday28kumar@gmail.com>
2026-02-21 16:19:08 +0530

    add api-gateway service with routing configuration and Docker setup

---
+++ patient-management/api-gateway/src/main/java/org/neev/apigateway/ApiGatewayApp.java
@@ -0,0 +1,12 @@
+package org.neev.apigateway;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApp.class, args);
+    }
+}
