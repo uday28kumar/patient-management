c095b008d1367a216710dad50c4ebd312548b1c8
Uday Kumar <uday28kumar@gmail.com>
2026-02-21 16:50:33 +0530

    enhance documentation for BillingGrpcService and BillingServiceApp classes to clarify their roles and functionalities

--- patient-management/billing-service/src/main/java/org/neev/billingservice/BillingServiceApp.java
+++ patient-management/billing-service/src/main/java/org/neev/billingservice/BillingServiceApp.java
@@ -3,6 +3,11 @@ package org.neev.billingservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main application class for the Billing Service. This service handles all billing-related operations, including
+ * creating bills, retrieving billing information, and managing payment records. It also listens to relevant events
+ * from Kafka to keep the billing data up-to-date.
+ */
 @SpringBootApplication
 public class BillingServiceApp
 {
--- patient-management/billing-service/src/main/java/org/neev/billingservice/service/BillingGrpcService.java
+++ patient-management/billing-service/src/main/java/org/neev/billingservice/service/BillingGrpcService.java
@@ -8,10 +8,21 @@ import org.neev.billing.grpc.BillingServiceGrpc;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * gRPC service implementation for handling billing-related requests. This service defines the logic for processing
+ * billing operations such as creating bills, retrieving billing information, and managing payment records.
+ */
 @GrpcService
 public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
     private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
 
+    /**
+     * Handles incoming billing creation requests. This method processes the billing information provided in the request
+     * and generates a response indicating the success or failure of the operation.
+     *
+     * @param request          the billing request containing necessary information for creating a bill
+     * @param responseObserver the stream observer used to send back the billing response to the client
+     */
     @Override
     public void createBilling(BillingRequest request, StreamObserver<BillingResponse> responseObserver) {
         log.info("Request = {}", request.toString());
